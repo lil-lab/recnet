7731fdd4ab8203d7e51b8768992520bf5579dec8
Anya Ji <aj592@cornell.edu>
2023-10-29 18:28:21 -0400

    fix links in about page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import styles from "@/styles/About.module.css";
-import { Typography, Divider } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Typography, Divider, Link } from "@mui/material";
 
 export default function About() {
   return (
@@ -16,14 +15,17 @@ export default function About() {
         About
       </Typography>
       <Typography variant="body1">
-        Recnet is a human-driven recommendation system for academic readings. 
-          Recnet implements a mechanism similar to contemporary social networks, 
-          but it is designed to be impoverished in certain ways through information 
-          bottlenecks that increase communication cost. This is intended to limit 
-          the amount of time the system consumes from its users, while increasing 
-          the quality of information passed. The Recnet mechanism was initially 
-          outlined by Yoav Artzi in <a href="https://yoavartzi.substack.com/p/a-quick-sketch-of-a-human-based-paper">a Substack post</a>.
-          Recnet is currently is initial development stages.
+        Recnet is a human-driven recommendation system for academic readings.
+        Recnet implements a mechanism similar to contemporary social networks,
+        but it is designed to be impoverished in certain ways through
+        information bottlenecks that increase communication cost. This is
+        intended to limit the amount of time the system consumes from its users,
+        while increasing the quality of information passed. The Recnet mechanism
+        was initially outlined by Yoav Artzi in{" "}
+        <Link href="https://yoavartzi.substack.com/p/a-quick-sketch-of-a-human-based-paper">
+          a Substack post
+        </Link>
+        . Recnet is currently in initial development stages.
       </Typography>
       <Divider
         textAlign="left"
@@ -37,9 +39,19 @@ export default function About() {
         Team
       </Typography>
       <Typography variant="body1">
-        Anya Ji [<a href="https://anya-ji.github.io/">website</a>] [<a href="https://www.recnet.io/profile?userId=LCiJ5IX9R0P8DxqyBFr28LkBsUq1">recnet profile</a>]
+        Anya Ji [<Link href="https://anya-ji.github.io/">website</Link>] [
+        <Link href="https://www.recnet.io/profile?userId=LCiJ5IX9R0P8DxqyBFr28LkBsUq1">
+          recnet profile
+        </Link>
+        ]
+        <br />
         Valene Tjong
-        Yoav Artzi [<a href="https://yoavartzi.com/">website</a>] [<a href="https://www.recnet.io/profile?userId=foiYV9JeW2fFUkJvbQ6KnIC1tT93">recnet profile</a>]
+        <br />
+        Yoav Artzi [<Link href="https://yoavartzi.com/">website</Link>] [
+        <Link href="https://www.recnet.io/profile?userId=foiYV9JeW2fFUkJvbQ6KnIC1tT93">
+          recnet profile
+        </Link>
+        ]
       </Typography>
     </main>
   );
