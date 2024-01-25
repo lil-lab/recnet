cc5c9207d318ec48f9fb619ed38af180be212a49
swh00tw <a6140000@gmail.com>
2024-01-25 06:53:39 -0500

    style: add radix theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>123 {children}</body>
+      <body className={inter.className}>
+        <Theme accentColor="blue">{children}</Theme>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
 export default function Home() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Lets go</Button>
+      </Flex>
+    </div>
+  );
 }
