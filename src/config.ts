2b150faac24cb3b33269143dbee049ae1448a7dd
swh00tw <a6140000@gmail.com>
2024-01-27 16:51:13 -0500

    fix typo

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const clientConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const serverConfig = {
   useSecureCookies: process.env.USE_SECURE_COOKIES === "true",
-  firebaseApiKey: process.env.FIREBASE_API_KEY!,
+  firebaseApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   serviceAccount: {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
