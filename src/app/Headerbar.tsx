9282bdbd9a211eacd2ca17fa1ef4d544753f940c
swh00tw <a6140000@gmail.com>
2024-01-27 18:25:20 -0500

    style(headerbar): finish headerbar rwd

--- src/app/Headerbar.tsx
+++ src/app/Headerbar.tsx
@@ -5,10 +5,13 @@ import { Button, Flex, Text, TextField } from "@radix-ui/themes";
 import Link from "next/link";
 import { logout, useGoogleLogin } from "@/firebase/auth";
 import { useAuth } from "@/app/AuthContext";
+import { MagnifyingGlassIcon, Cross1Icon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export function Headerbar() {
   const { login } = useGoogleLogin();
   const { user } = useAuth();
+  const [enableSearch, setEnableSearch] = useState(false);
 
   const handleLogin = async () => {
     await login();
@@ -26,7 +29,8 @@ export function Headerbar() {
         "w-full",
         `h-[64px]`,
         "justify-between",
-        "px-16",
+        "px-4",
+        "md:px-16",
         "py-[10px]",
         "shadow-4",
         "bg-white",
@@ -38,17 +42,72 @@ export function Headerbar() {
         <Text className={cn("text-blue-10", "font-bold")} size="8">
           RecNet
         </Text>
-        <TextField.Root>
+        <TextField.Root className="hidden md:inline-block">
           <TextField.Input placeholder="Search the users..." size="2" />
         </TextField.Root>
       </Flex>
       <Flex className="items-center" gap="4">
-        <Link href="/about">
+        <MagnifyingGlassIcon
+          width="20"
+          height="20"
+          onClick={() => {
+            setEnableSearch(!enableSearch);
+          }}
+          className={cn(
+            {
+              hidden: enableSearch,
+              "inline-block": !enableSearch,
+            },
+            "md:hidden"
+          )}
+        />
+        <TextField.Root
+          className={cn(
+            {
+              hidden: !enableSearch,
+              flex: enableSearch,
+            },
+            "transition-all",
+            "ease-in-out",
+            "duration-200"
+          )}
+        >
+          <TextField.Input placeholder="Search the users..." size="2" />
+          <TextField.Slot>
+            <Cross1Icon
+              width="16"
+              height="16"
+              onClick={() => {
+                setEnableSearch(false);
+              }}
+              className={cn(
+                {
+                  hidden: !enableSearch,
+                  "inline-block": enableSearch,
+                },
+                "md:hidden"
+              )}
+            />
+          </TextField.Slot>
+        </TextField.Root>
+        <Link
+          href="/about"
+          className={cn({
+            hidden: enableSearch,
+            "inline-block": !enableSearch,
+          })}
+        >
           <Text size="3" weight="medium">
             About
           </Text>
         </Link>
-        <Link href="/faq">
+        <Link
+          href="/faq"
+          className={cn({
+            hidden: enableSearch,
+            "inline-block": !enableSearch,
+          })}
+        >
           <Text size="3" weight="medium">
             FAQ
           </Text>
