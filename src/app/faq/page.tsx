30e21b113726ac3c111ebc6cad431a2e7129db1f
swh00tw <a6140000@gmail.com>
2024-01-27 23:29:06 -0500

    feat: finish faq page

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/utils/cn";
+import { getNextCutOff, getVerboseDateString } from "@/utils/date";
+import { Text } from "@radix-ui/themes";
+
+const faqs: {
+  title: string;
+  content: string | (() => string);
+}[] = [
+  {
+    title: "How to use it?",
+    content:
+      "Each user may recommend one paper per cycle. They may change the paper they recommend each cycle as many times as they want during the cycle (i.e, before the cutoff time). A recommendation is made out of (a) a link to the paper, (b) paper title and authors, (c) a very short tl;dr message of 280 characters.",
+  },
+  {
+    title: "Cycle",
+    content: () => {
+      const nextCutOff = getNextCutOff();
+      return `The cutoff time of each cycle is 11:59:59 PM GMT on each Tuesday. The next cutoff in your local time: ${getVerboseDateString(nextCutOff)}.`;
+    },
+  },
+];
+
+export default function AboutPage() {
+  return (
+    <div
+      className={cn(
+        "w-full",
+        "lg:w-[60%]",
+        `min-h-[90svh]`,
+        "flex",
+        "flex-col",
+        "p-8",
+        "gap-y-6"
+      )}
+    >
+      <Text size="8" className="text-gray-12" weight="bold">
+        FAQ
+      </Text>
+      {faqs.map((faq) => (
+        <>
+          <Text className="text-gray-12" size="7" weight="medium">
+            {faq.title}
+          </Text>
+          <Text className="text-gray-11" size="4">
+            {typeof faq.content === "string" ? faq.content : faq.content()}
+          </Text>
+        </>
+      ))}
+    </div>
+  );
+}
---
+++ src/utils/date.ts
@@ -0,0 +1,34 @@
+import { nextTuesday, set } from "date-fns";
+import next from "next";
+
+const CYCLE_DUE_DAY = 2;
+
+export const getNextCutOff = (): Date => {
+  const currentDate = new Date();
+  const currentDay = currentDate.getUTCDay();
+  const daysUntilNextCutoff = (CYCLE_DUE_DAY + 7 - currentDay) % 7;
+  currentDate.setUTCDate(currentDate.getUTCDate() + daysUntilNextCutoff);
+  currentDate.setUTCHours(23, 59, 59, 999);
+  return currentDate;
+};
+
+export const getLatestCutOff = (): Date => {
+  const nextCutOff = getNextCutOff();
+  nextCutOff.setUTCDate(nextCutOff.getUTCDate() - 7);
+  return nextCutOff;
+};
+
+export const getVerboseDateString = (date: Date): string => {
+  return Intl.DateTimeFormat("default", {
+    weekday: "long",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+    hourCycle: "h11",
+  })
+    .format(date)
+    .replaceAll(",", " ");
+};
