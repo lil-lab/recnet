421f7a7673746276d3d047961a36e8b43646814e
swh00tw <a6140000@gmail.com>
2024-02-04 00:24:13 -0500

    feat: finish profile page

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -4,6 +4,7 @@ import { getUserByUsername } from "@/server/user";
 import { notFound } from "next/navigation";
 import { getRecsByUserId } from "@/server/rec";
 import { RecCard } from "@/components/RecCard";
+import { Text } from "@radix-ui/themes";
 
 export default async function UserProfilePage({
   params,
@@ -34,10 +35,19 @@ export default async function UserProfilePage({
     >
       <Profile username={username} />
       <div className="w-full h-[1px] bg-gray-8" />
-      {/* TODO: render user old recs */}
-      {recs.map((rec, idx) => {
-        return <RecCard key={`${rec.title}-${rec.userId}-${idx}`} rec={rec} />;
-      })}
+      {recs.length > 0 ? (
+        recs.map((rec, idx) => {
+          return (
+            <RecCard key={`${rec.title}-${rec.userId}-${idx}`} rec={rec} />
+          );
+        })
+      ) : (
+        <div className="h-[150px] w-full flex justify-center items-center">
+          <Text size="3" className="text-gray-10">
+            No recommendations yet.
+          </Text>
+        </div>
+      )}
     </div>
   );
 }
