8c27b3751766f12453c3449a937676139d945cf9
swh00tw <a6140000@gmail.com>
2024-02-06 11:08:08 -0500

    style: add sf pro font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { Theme } from "@radix-ui/themes";
 import { Headerbar } from "@/app/Headerbar";
 import { AuthProvider } from "./AuthProvider";
@@ -10,7 +10,101 @@ import { HistoryProvider } from "./HistoryProvider";
 import "@radix-ui/themes/styles.css";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const sfpro = localFont({
+  src: [
+    {
+      path: "../fonts/SF-Pro-Text-Black.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Heavy.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Thin.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-Ultralight.otf",
+      weight: "100",
+      style: "normal",
+    },
+    // italic
+    {
+      path: "../fonts/SF-Pro-Text-BlackItalic.otf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-HeavyItalic.otf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-BoldItalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-SemiboldItalic.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-MediumItalic.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-RegularItalic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-LightItalic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-ThinItalic.otf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../fonts/SF-Pro-Text-UltralightItalic.otf",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "RecNet",
@@ -25,7 +119,7 @@ export default async function RootLayout({
   const user = await getUserServerSide();
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={sfpro.className}>
         <AuthProvider serverUser={user}>
           <HistoryProvider>
             <Theme accentColor="blue">
