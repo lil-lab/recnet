ffdbd0218dff89d9999f491edda21296f1d6edc8
swh00tw <a6140000@gmail.com>
2024-02-08 12:24:26 -0500

    fix: fix wrong private key format

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const serverConfig = {
   serviceAccount: {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/gm, "\n"),
   },
 };
 
