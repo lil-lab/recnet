e37e7420a37c61f3973d8d78ac01bfc3db9607bb
swh00tw <a6140000@gmail.com>
2024-02-18 18:42:45 -0500

    chore: remove unused files

--- apps/recnet-email/src/lib/recnet-email.spec.ts
+++
@@ -1,7 +0,0 @@
-import { recnetEmail } from "./recnet-email";
-
-describe("recnetEmail", () => {
-  it("should work", () => {
-    expect(recnetEmail()).toEqual("recnet-email");
-  });
-});
--- apps/recnet-email/src/lib/recnet-email.ts
+++
@@ -1,3 +0,0 @@
-export function recnetEmail(): string {
-  return "recnet-email";
-}
