5a4c32238b9626ee18e11f4bfec73b75c286af9b
swh00tw <a6140000@gmail.com>
2024-02-21 18:49:08 -0500

    feat: add announcement banner
    #146

---
+++ apps/recnet/src/app/AnnouncementBanner.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { cn } from "@/utils/cn";
+import { Text } from "@radix-ui/themes";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
+export function AnnouncementBanner() {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div
+      className={cn(
+        "fixed",
+        "top-[64px]",
+        "left-0",
+        "w-full",
+        "bg-gradient-to-b",
+        "from-[#0091FF]",
+        "to-[#0091FF50]",
+        "py-3",
+        "flex",
+        "flex-row",
+        "justify-between",
+        "items-center",
+        "text-white",
+        {
+          hidden: !isOpen,
+        }
+      )}
+    >
+      <div />
+      <Text size="2" weight="medium">
+        📢 RecNet is under early development. Signing up requires an invite
+        code.
+      </Text>
+      <div
+        className="mr-4 cursor-pointer"
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      >
+        <Cross1Icon width="20" height="20" />
+      </div>
+    </div>
+  );
+}
