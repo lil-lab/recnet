374a472fd1346dcbdd7e19d83a774ebcae40512f
joannechen1223 <joannechen1223@gmail.com>
2024-02-24 02:34:14 -0500

    test: add test for health service

---
+++ apps/recnet-api/src/modules/health/health.service.spec.ts
@@ -0,0 +1,14 @@
+import { HealthService } from './health.service';
+
+describe('HealthService', () => {
+  let healthService: HealthService;
+
+  beforeEach(async () => {
+    healthService = new HealthService();
+  });
+
+  it('should return status ok', async () => {
+    const result = await healthService.getHealth();
+    expect(result).toEqual({ status: 'OK' });
+  });
+});
