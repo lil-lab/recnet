88642c3e5a111134b02ca491dbe283e28f03a19c
swh00tw <a6140000@gmail.com>
2024-02-29 18:47:59 -0500

    feat: add comment

--- apps/recnet/src/utils/withSuspense.tsx
+++ apps/recnet/src/utils/withSuspense.tsx
@@ -1,5 +1,7 @@
 import React, { ComponentType, Suspense, SuspenseProps } from "react";
 
+// React Server Component HOC for Suspense
+// ref: https://gist.github.com/emeraldsanto/43ae63eff64cafbab58d6e4d740deabf
 export function withSuspense<P extends object>(
   WrappedComponent: ComponentType<P>,
   fallback: SuspenseProps["fallback"] = null
