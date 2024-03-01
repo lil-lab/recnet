dcac85392f80c5a5ec132ffef54a3d7cedb02aef
joannechen1223 <joannechen1223@gmail.com>
2024-02-29 23:03:21 -0500

    chore: add swagger api doc

--- apps/recnet-api/src/main.ts
+++ apps/recnet-api/src/main.ts
@@ -4,6 +4,7 @@
  */
 
 import { Logger } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
@@ -11,6 +12,18 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const nodeEnv = process.env.NODE_ENV || "development";
+
+  if (nodeEnv === "development" || nodeEnv === "local") {
+    const config = new DocumentBuilder()
+      .setTitle("Recnet API")
+      .setDescription("This is the Recnet API document.")
+      .setVersion("1.0")
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+  }
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
