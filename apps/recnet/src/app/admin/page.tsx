87a6220f5ab48073ab750c5ffc10b941fa3700d6
swh00tw <a6140000@gmail.com>
2024-03-04 15:22:37 -0500

    Redirect to user-rec stats page in admin page

--- apps/recnet/src/app/admin/page.tsx
+++ apps/recnet/src/app/admin/page.tsx
@@ -1,7 +1,14 @@
 import { cn } from "@/utils/cn";
+import { redirect } from "next/navigation";
 
+// Leave this page here, redirect to user-rec stats for now
+// We would want to build admin main page in the future
 export default function AdminPage() {
+  redirect("/admin/stats/user-rec");
+
   return (
-    <div className={cn("w-full", "flex", "justify-center")}>Admin Page</div>
+    <div className={cn("w-full", "flex", "justify-center")}>
+      To be implemented
+    </div>
   );
 }
