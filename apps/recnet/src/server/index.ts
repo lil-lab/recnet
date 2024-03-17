6ac043e12708562483fffdbc4df9671b9f7e5863
swh00tw <a6140000@gmail.com>
2024-03-17 16:25:28 -0400

    feat: add user router

--- apps/recnet/src/server/index.ts
+++ apps/recnet/src/server/index.ts
@@ -1,7 +1,8 @@
 import * as trpc from "@trpc/server";
 import { mergeRouters } from "./trpc";
 import { publicRouter } from "./routers/public";
+import { userRouter } from "./routers/user";
 
-export const appRouter = mergeRouters(publicRouter);
+export const appRouter = mergeRouters(publicRouter, userRouter);
 
 export type AppRouter = typeof appRouter;
