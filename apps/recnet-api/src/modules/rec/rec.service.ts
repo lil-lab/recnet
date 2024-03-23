8291c4a985ff9a2727f60fe1bf401ffea406354f
swh00tw <a6140000@gmail.com>
2024-03-23 17:33:08 -0400

    fix: fix wronog response data type

--- apps/recnet-api/src/modules/rec/rec.service.ts
+++ apps/recnet-api/src/modules/rec/rec.service.ts
@@ -26,7 +26,12 @@ export class RecService {
         ...dbRec,
         cutoff: dbRec.cutoff.toISOString(),
         user: {
-          ...dbRec.user,
+          id: dbRec.user.id,
+          handle: dbRec.user.handle,
+          displayName: dbRec.user.displayName,
+          photoUrl: dbRec.user.photoUrl,
+          affiliation: dbRec.user.affiliation,
+          bio: dbRec.user.bio,
           numFollowers: dbRec.user.followedBy.length,
         },
         article: {
