f6c924beb6d46498091a827df66ab8a7f1bf6ff1
swh00tw <a6140000@gmail.com>
2024-04-01 00:00:15 -0400

    feat: add docs

---
+++ apps/recnet-docs/next.config.js
@@ -0,0 +1,34 @@
+//@ts-check
+
+// ref: https://github.com/shuding/nextra/issues/454#issuecomment-2028475942
+if (process.cwd() !== __dirname) {
+  process.chdir(__dirname);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require("@nx/next");
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNextra,
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
---
+++ apps/recnet-docs/theme.config.jsx
@@ -0,0 +1,9 @@
+const config = {
+  logo: <span>RecNet Docs</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
+
+export default config;
