0eb76b14bb6cd754e7f73f737b58178daf6beb6e
swh00tw <a6140000@gmail.com>
2024-04-06 15:52:09 -0400

    feat: finish search api

--- apps/recnet/src/app/all-users/page.tsx
+++ apps/recnet/src/app/all-users/page.tsx
@@ -1,13 +1,44 @@
-import { Text } from "@radix-ui/themes";
+"use client";
 
-import { serverClient } from "@recnet/recnet-web/app/_trpc/serverClient";
+import { Text } from "@radix-ui/themes";
+import { useMemo } from "react";
+
 import { NotFoundBlock } from "@recnet/recnet-web/app/search/NotFound";
 import { GoBackButton } from "@recnet/recnet-web/components/GoBackButton";
 import { UserList } from "@recnet/recnet-web/components/UserCard";
 import { cn } from "@recnet/recnet-web/utils/cn";
 
-export default async function SearchResultPage() {
-  const { users } = await serverClient.search({ keyword: "" });
+import { useAuth } from "../AuthContext";
+import { trpc } from "../_trpc/client";
+import { getShuffledUsersFromInfiniteQuery } from "../search/page";
+
+export default function SearchResultPage() {
+  const { user } = useAuth();
+  const { data, isPending, fetchNextPage } = trpc.search.useInfiniteQuery(
+    {
+      keyword: "",
+      pageSize: 20,
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (!lastPage.hasNext) {
+          return null;
+        }
+        return allPages.length + 1;
+      },
+      initialCursor: 1,
+    }
+  );
+
+  const users = useMemo(
+    () => getShuffledUsersFromInfiniteQuery(data, user?.id),
+    [data, user]
+  );
+
+  if (isPending) {
+    // TODO: Add loading animation
+    return <div>Loading...</div>;
+  }
 
   return (
     <div
--- apps/recnet/src/app/search/page.tsx
+++ apps/recnet/src/app/search/page.tsx
@@ -1,21 +1,68 @@
-import { Text } from "@radix-ui/themes";
+"use client";
 
-import { serverClient } from "@recnet/recnet-web/app/_trpc/serverClient";
+import { Button, Text } from "@radix-ui/themes";
+import { InfiniteData } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+import { useAuth } from "@recnet/recnet-web/app/AuthContext";
 import { GoBackButton } from "@recnet/recnet-web/components/GoBackButton";
 import { UserList } from "@recnet/recnet-web/components/UserCard";
 import { cn } from "@recnet/recnet-web/utils/cn";
+import { shuffleArray } from "@recnet/recnet-web/utils/shuffle";
+
+import { GetUsersResponse, UserPreview } from "@recnet/recnet-api-model";
 
 import { NotFoundBlock } from "./NotFound";
 
-export default async function SearchResultPage({
+import { trpc } from "../_trpc/client";
+
+export const getShuffledUsersFromInfiniteQuery = (
+  infiniteQueryData: InfiniteData<GetUsersResponse> | undefined,
+  shuffleKey: string | undefined
+) => {
+  if (!infiniteQueryData) {
+    return [];
+  }
+  return (infiniteQueryData?.pages ?? []).reduce((acc, page) => {
+    const newBatch = shuffleArray(page.users, shuffleKey || "");
+    return [...acc, ...newBatch];
+  }, [] as UserPreview[]);
+};
+
+export default function SearchResultPage({
   searchParams,
 }: {
   searchParams: {
     q: string;
   };
 }) {
+  const { user } = useAuth();
   const query = searchParams["q"];
-  const { users } = await serverClient.search({ keyword: query });
+  const { data, isPending, fetchNextPage } = trpc.search.useInfiniteQuery(
+    {
+      keyword: query,
+      pageSize: 20,
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (!lastPage.hasNext) {
+          return null;
+        }
+        return allPages.length + 1;
+      },
+      initialCursor: 1,
+    }
+  );
+
+  const users = useMemo(
+    () => getShuffledUsersFromInfiniteQuery(data, user?.id),
+    [data, user]
+  );
+
+  if (isPending) {
+    // TODO: Add loading animation
+    return <div>Loading...</div>;
+  }
 
   return (
     <div
@@ -31,10 +78,17 @@ export default async function SearchResultPage({
       )}
     >
       <GoBackButton />
-      <Text
-        size="7"
-        className="text-gray-12 font-medium"
-      >{`${users.length} result${users.length > 1 ? "s" : ""}`}</Text>
+      <Button
+        className="w-full"
+        onClick={() => {
+          fetchNextPage();
+        }}
+      >
+        Load More
+      </Button>
+      <Text size="7" className="text-gray-12 font-medium">{`${
+        users.length
+      } result${users.length > 1 ? "s" : ""}`}</Text>
       {users.length === 0 ? <NotFoundBlock /> : <UserList users={users} />}
     </div>
   );
