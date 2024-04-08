5083b3c99ed5fa265f91ee6e1e1d6e8eea60552a
swh00tw <a6140000@gmail.com>
2024-04-08 13:40:08 -0400

    Fix cleanupTest task and update genRS256KeyPair.sh script

--- libs/recnet-jwt/scripts/genRS256KeyPair.sh
+++ libs/recnet-jwt/scripts/genRS256KeyPair.sh
@@ -1,16 +1,19 @@
+#!/bin/sh
+
+# Generate RSA key pair
 ssh-keygen -t rsa -b 4096 -m PEM -f jwtRS256.key -N ""
 # Don't add passphrase
 openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
 
-# find the current directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Find the current directory of this script
+DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: $DIR"
 
-# put them in .env file under the current directory
-echo "PRIVATE_KEY='$(cat jwtRS256.key)'" > $(pwd)/.env.local
-echo "PUBLIC_KEY='$(cat jwtRS256.key.pub)'" >> $(pwd)/.env.local
+# Put them in .env file under the current directory
+echo "PRIVATE_KEY='$(cat jwtRS256.key)'" > "$DIR/.env.local"
+echo "PUBLIC_KEY='$(cat jwtRS256.key.pub)'" >> "$DIR/.env.local"
 
 echo "Generated jwtRS256.key and jwtRS256.key.pub and put them in .env.local file."
-# clean up
-rm jwtRS256.key
-rm jwtRS256.key.pub
+# Clean up
+rm "jwtRS256.key"
+rm "jwtRS256.key.pub"
