ad1e5296ad66ec504cffa01f01754f35812eeffe
swh00tw <a6140000@gmail.com>
2024-04-08 01:44:38 -0400

    Add new pages and update meta.json files

--- apps/recnet-docs/theme.config.jsx
+++ apps/recnet-docs/theme.config.jsx
@@ -3,6 +3,14 @@ const config = {
   project: {
     link: "https://github.com/lil-lab/recnet",
   },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === "separator") {
+        return <div>{title}</div>;
+      }
+      return <>{title}</>;
+    },
+  },
   // ... other theme options
 };
 
