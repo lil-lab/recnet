2a24f1e347f341575968dacca6fbc226eb463c4c
swh00tw <a6140000@gmail.com>
2024-04-08 09:56:08 -0400

    fix: fix recnet-jwt test ci

--- libs/recnet-jwt/scripts/genRS256KeyPair.sh
+++ libs/recnet-jwt/scripts/genRS256KeyPair.sh
@@ -6,9 +6,9 @@ openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "DIR: $DIR"
 
-# put them in .env file
-echo "PRIVATE_KEY='$(cat jwtRS256.key)'" > .env.local
-echo "PUBLIC_KEY='$(cat jwtRS256.key.pub)'" >> .env.local
+# put them in .env file under the current directory
+echo "PRIVATE_KEY='$(cat jwtRS256.key)'" > $(pwd)/.env.local
+echo "PUBLIC_KEY='$(cat jwtRS256.key.pub)'" >> $(pwd)/.env.local
 
 echo "Generated jwtRS256.key and jwtRS256.key.pub and put them in .env.local file."
 # clean up
