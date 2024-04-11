fc98b1a65e3135aee5d6a0cc7d810961ec3e458d
swh00tw <a6140000@gmail.com>
2024-04-11 18:51:27 -0400

    build(recnet-docs): use symlink instead of copy

--- apps/recnet-docs/next.config.js
+++ apps/recnet-docs/next.config.js
@@ -4,17 +4,18 @@ const path = require("path");
 
 console.log("cwd: ", process.cwd());
 
-// if pages directory does not exist
-const pagesDir = path.resolve(__dirname, "./src/pages");
-const rootPagesDir = path.resolve(__dirname, "../../pages");
-if (!fs.existsSync(rootPagesDir)) {
-  // copy all files from pageDir to rootPagesDir
-  console.log(`Copying ${pagesDir} to ${rootPagesDir}`);
-  fs.mkdirSync(rootPagesDir);
-  fs.cpSync(pagesDir, rootPagesDir, {
-    recursive: true,
-  });
-}
+// ref: https://github.com/shuding/nextra/issues/454#issuecomment-1907150823
+const destPagesPath = path.join(__dirname, "../../pages");
+fs.symlink("apps/recnet-docs/src/pages", destPagesPath, "dir", (e) => {
+  console.log("Created symlink to apps/recnet-docs/src/pages in pages/");
+  console.log(
+    "Learn more: ",
+    "https://github.com/shuding/nextra/issues/454#issuecomment-1907150823"
+  );
+  if (e && e.message.startsWith("EEXIST")) {
+    console.log("ℹ️ Symlink already exists. Skipping symlink creation.");
+  }
+});
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require("@nx/next");
