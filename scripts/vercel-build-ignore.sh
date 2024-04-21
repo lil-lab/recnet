8a930681986e0e223b2187041a5c743649b6037e
swh00tw <a6140000@gmail.com>
2024-04-21 01:34:50 -0400

    fix: fix bug

--- scripts/vercel-build-ignore.sh
+++ scripts/vercel-build-ignore.sh
@@ -16,7 +16,7 @@ pnpm install
 # Check affected projects, looking for the absence of the provided string
 echo "ℹ️ Checking affected projects..."
 echo "Affected projects: "
-pnpm nx show projects --affected --base=$VERCEL_GIT_PREVIOUS_SHA --head=$VERCEL_GIT_COMMIT_SHA | (grep -x "$search_string")
+pnpm nx show projects --affected true --base=$VERCEL_GIT_PREVIOUS_SHA --head=$VERCEL_GIT_COMMIT_SHA | (grep -x "$search_string")
 
 # Capture the exit status of the grep command
 exit_status=$?
