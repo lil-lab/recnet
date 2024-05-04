b88d676b9ce139ac1b1969c5863f8218445b8ac5
joannechen1223 <joannechen1223@gmail.com>
2024-05-04 00:11:12 -0400

    chore: extract sleep duration to const

--- apps/recnet-api/src/modules/email/email.const.ts
+++ apps/recnet-api/src/modules/email/email.const.ts
@@ -4,5 +4,7 @@ export const WEEKLY_DIGEST_CRON = "0 0 0 * * 3";
 
 export const RETRY_LIMIT = 3;
 
+export const SLEEP_DURATION_MS = 1000;
+
 // providers
 export const MAIL_TRANSPORTER = "MAIL_TRANSPORTER";
