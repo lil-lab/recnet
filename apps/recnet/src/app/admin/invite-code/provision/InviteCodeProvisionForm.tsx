f14021c6932d53c6ba5cd069e186326628738510
swh00tw <a6140000@gmail.com>
2024-05-08 21:59:34 -0400

    style: remove icon

--- apps/recnet/src/app/admin/invite-code/provision/InviteCodeProvisionForm.tsx
+++ apps/recnet/src/app/admin/invite-code/provision/InviteCodeProvisionForm.tsx
@@ -77,11 +77,7 @@ export function InviteCodeProvisionForm() {
             {...register("upperBound", {
               setValueAs: (val) => (val === "" ? null : val),
             })}
-          >
-            <TextField.Slot>
-              <AtSignIcon size="12" className="text-gray-10" />
-            </TextField.Slot>
-          </TextField.Root>
+          ></TextField.Root>
           {formState.errors.upperBound ? (
             <Text size="1" color="red">
               {formState.errors.upperBound.message}
