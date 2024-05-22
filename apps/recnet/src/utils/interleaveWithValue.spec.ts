77ef245c67c6b0bb610167762a1b5628327a3a4b
swh00tw <a6140000@gmail.com>
2024-05-21 22:55:13 -0400

    test(recnet): add utils unit test

--- apps/recnet/src/utils/interleaveWithValue.spec.ts
+++ apps/recnet/src/utils/interleaveWithValue.spec.ts
@@ -1,25 +1,23 @@
-import { expect, test, describe } from "vitest";
+import { expect, test } from "vitest";
 
 import { interleaveWithValue } from "./interleaveWithValue";
 
-describe("Number", () => {
-  test("Should interleave with value", () => {
-    const result = interleaveWithValue([1, 2, 3, 4], 0);
-    expect(result).toEqual([1, 0, 2, 0, 3, 0, 4]);
-  });
+test("Should interleave with value: Number", () => {
+  const result = interleaveWithValue([1, 2, 3, 4], 0);
+  expect(result).toEqual([1, 0, 2, 0, 3, 0, 4]);
+});
 
-  test("Should interleave with value", () => {
-    const result = interleaveWithValue([1, 2, 3, 4], 1);
-    expect(result).toEqual([1, 1, 2, 1, 3, 1, 4]);
-  });
+test("Should interleave with value: String", () => {
+  const result = interleaveWithValue(["a", "b", "c", "d"], "z");
+  expect(result).toEqual(["a", "z", "b", "z", "c", "z", "d"]);
+});
 
-  test("Should not interleave anything if the target array is empty", () => {
-    const result = interleaveWithValue([], 1);
-    expect(result).toEqual([]);
-  });
+test("Should not interleave anything if the target array is empty", () => {
+  const result = interleaveWithValue([], 1);
+  expect(result).toEqual([]);
+});
 
-  test("Should not interleave anything if the target array has length equal to one", () => {
-    const result = interleaveWithValue([1], 1);
-    expect(result).toEqual([1]);
-  });
+test("Should not interleave anything if the target array has length equal to one", () => {
+  const result = interleaveWithValue([1], 1);
+  expect(result).toEqual([1]);
 });
