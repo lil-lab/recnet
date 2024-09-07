1a08aeaa9d87a2fb31a59f4276c0f7541425fed7
swh00tw <a6140000@gmail.com>
2024-09-07 17:49:11 -0400

    feat: add new field to rec table: isSelfRec

---
+++ apps/recnet-api/prisma/migrations/20240907214307_add_self_rec_flag_into_rec_model/down.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Recommendation" DROP COLUMN "isSelfRec";
+
---
+++ apps/recnet-api/prisma/migrations/20240907214307_add_self_rec_flag_into_rec_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Recommendation" ADD COLUMN     "isSelfRec" BOOLEAN NOT NULL DEFAULT false;
