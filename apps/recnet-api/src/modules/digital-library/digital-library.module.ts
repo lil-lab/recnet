b70c3a6a1574789be6d76ea08ff701c8cbde51eb
joannechen1223 <joannechen1223@gmail.com>
2024-09-15 22:12:02 -0400

    feat: add digital library service factory

--- apps/recnet-api/src/modules/digital-library/digital-library.module.ts
+++ apps/recnet-api/src/modules/digital-library/digital-library.module.ts
@@ -1,24 +1,19 @@
-import { Module } from "@nestjs/common";
+import { Module, Scope } from "@nestjs/common";
+import { REQUEST } from "@nestjs/core";
 
 import { DbRepositoryModule } from "@recnet-api/database/repository/db.repository.module";
 import DigitalLibraryRepository from "@recnet-api/database/repository/digital-library.repository";
 
-import { ArXivService } from "./arxiv.service";
-import { DigitalLibraryService } from "./digital-library.service";
+import { DigitalLibraryServiceFactory } from "./digital-library.service";
 import { DIGITAL_LIBRARY } from "./digital-libray.const";
-
-const digitalLibraryFactory = (
-  digitalLibraryRepository: DigitalLibraryRepository
-): DigitalLibraryService => {
-  return new ArXivService(digitalLibraryRepository, 2);
-};
 
 @Module({
   providers: [
     {
       provide: DIGITAL_LIBRARY,
-      useFactory: digitalLibraryFactory,
-      inject: [DigitalLibraryRepository],
+      useFactory: DigitalLibraryServiceFactory,
+      inject: [DigitalLibraryRepository, REQUEST],
+      scope: Scope.REQUEST,
     },
   ],
   imports: [DbRepositoryModule],
--- apps/recnet-api/src/modules/digital-library/digital-library.service.ts
+++ apps/recnet-api/src/modules/digital-library/digital-library.service.ts
@@ -1,5 +1,46 @@
+import { Request } from "express";
+
+import DigitalLibraryRepository from "@recnet-api/database/repository/digital-library.repository";
+
+import { ArXivService } from "./arxiv.service";
+import { DIGITAL_LIBRARY_ID } from "./digital-libray.const";
 import { Metadata } from "./entities/metadata.entity";
 
 export interface DigitalLibraryService {
   getMetadata(url: string): Promise<Metadata>;
 }
+
+const shouldProvideService = (req: Request) => {
+  return req.originalUrl.includes("article") && req.method === "GET";
+};
+
+export const DigitalLibraryServiceFactory = async (
+  digitalLibraryRepository: DigitalLibraryRepository,
+  req: Request
+): Promise<DigitalLibraryService | null> => {
+  if (!shouldProvideService(req)) {
+    return null;
+  }
+
+  const { link } = req.query;
+  if (typeof link !== "string") {
+    return null;
+  }
+
+  const digitalLibraries = await digitalLibraryRepository.findAll();
+
+  const matchingLibrary = digitalLibraries.find((library) =>
+    library.regex.some((pattern) => new RegExp(pattern).test(link))
+  );
+
+  if (!matchingLibrary) {
+    return null;
+  }
+
+  const matchId = matchingLibrary.id;
+  if (matchId === DIGITAL_LIBRARY_ID.arXiv) {
+    return new ArXivService(digitalLibraryRepository, 1);
+  }
+
+  return null;
+};
