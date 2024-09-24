9c309c22f881c5faab7162595462cc184a93a792
swh00tw <a6140000@gmail.com>
2024-09-24 11:09:37 -0400

    chore: change back

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,7 +11,7 @@ module.exports = {
       return `nx format:check --files=${files.join(",")}`;
     },
   ],
-  "{libs,tools}/**/*.{ts,tsx,json}": [
+  "{libs,tools}/**/*.{ts,tsx}": [
     (files) => {
       return `nx affected:lint --fix --files=${files.join(",")}`;
     },
