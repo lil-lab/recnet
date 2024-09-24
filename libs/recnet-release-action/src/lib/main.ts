626336b31089d51a959649ba58bf6c98dbae6224
swh00tw <a6140000@gmail.com>
2024-09-24 10:45:13 -0400

    chore: init project

---
+++ libs/recnet-release-action/src/lib/main.ts
@@ -0,0 +1,9 @@
+import * as core from "@actions/core";
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  core.info("Hello, world!");
+}
