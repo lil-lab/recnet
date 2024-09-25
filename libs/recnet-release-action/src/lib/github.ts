c285aebca9f8db3a53a1b4a6bca3a5ae9ec4c59e
swh00tw <a6140000@gmail.com>
2024-09-24 23:01:46 -0400

    chore: test

---
+++ libs/recnet-release-action/src/lib/github.ts
@@ -0,0 +1,173 @@
+import * as core from "@actions/core";
+import { Octokit } from "@octokit/core";
+import { Endpoints } from "@octokit/types";
+
+import { inputs } from "./env";
+
+type FoundPR =
+  Endpoints["GET /repos/{owner}/{repo}/pulls"]["response"]["data"][number];
+type CreatedPR =
+  Endpoints["POST /repos/{owner}/{repo}/pulls"]["response"]["data"];
+export type PR = FoundPR | CreatedPR;
+
+export type REF =
+  Endpoints["GET /repos/{owner}/{repo}/git/ref/{ref}"]["response"]["data"];
+
+export type Commit =
+  Endpoints["GET /repos/{owner}/{repo}/commits/{ref}"]["response"]["data"];
+
+export class GitHubAPI {
+  private octokit: Octokit;
+  private owner: string;
+  private repo: string;
+
+  constructor(token: string, owner: string, repo: string) {
+    this.octokit = new Octokit({ auth: token });
+    this.owner = owner;
+    this.repo = repo;
+  }
+
+  async findPRCreatedByBot(
+    baseBranch: string,
+    headBranch: string
+  ): Promise<FoundPR | null> {
+    const { data } = await this.octokit.request(
+      "GET /repos/{owner}/{repo}/pulls",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        state: "open",
+        head: `${this.owner}:${headBranch}`,
+        base: baseBranch,
+      }
+    );
+    // Filter PRs created by GitHub Actions
+    const actionCreatedPRs = data.filter(
+      (pr) =>
+        pr.user?.login === "github-actions[bot]" || // Check if created by GitHub Actions bot
+        pr.labels.some((label) => label.name === "automated-pr") // Or check for a specific label
+    );
+    core.debug(
+      `Found PRs created by GitHub Actions: ${actionCreatedPRs.length}`
+    );
+    core.debug(`PRs: ${JSON.stringify(actionCreatedPRs)}`);
+
+    return actionCreatedPRs.length > 0 ? actionCreatedPRs[0] : null;
+  }
+
+  async createPR(
+    title: string,
+    baseBranch: string,
+    headBranch: string,
+    body: string
+  ): Promise<CreatedPR> {
+    const { data } = await this.octokit.request(
+      "POST /repos/{owner}/{repo}/pulls",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        title,
+        head: headBranch,
+        base: baseBranch,
+        body,
+      }
+    );
+    return data;
+  }
+
+  async getLatestCommits(headBranch: string): Promise<Commit[]> {
+    // Find the commit where the "staging" tag is on
+    const { data: taggedCommit } = await this.octokit.request(
+      "GET /repos/{owner}/{repo}/commits/{ref}",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        ref: inputs.ref,
+      }
+    );
+    // get the date of the tagged commit
+    const tag = taggedCommit as Commit;
+    const commitDateTs = tag.commit.author?.date;
+    if (!commitDateTs) {
+      throw new Error("Could not find the commit date of the staging tag");
+    }
+
+    // Get commits after the latest "staging" tag
+    const { data: commits } = await this.octokit.request(
+      "GET /repos/{owner}/{repo}/commits",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        sha: headBranch,
+        since: commitDateTs,
+      }
+    );
+
+    return commits;
+  }
+
+  async appendIssuesToPR(originalPR: PR, issues: Set<string>) {
+    const issuesList = Array.from(issues)
+      .map((issue) => `- ${issue}`)
+      .join("\n");
+    const updatedBody = `${originalPR.body}\n${issuesList}`;
+
+    await this.octokit.request(
+      "PATCH /repos/{owner}/{repo}/pulls/{pull_number}",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        pull_number: originalPR.number,
+        body: updatedBody,
+      }
+    );
+  }
+
+  async requestReviewers(prNumber: number, reviewers: string[]) {
+    await this.octokit.request(
+      "POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        pull_number: prNumber,
+        reviewers,
+      }
+    );
+  }
+
+  async addAssignees(issueNumber: number, assignees: string[]) {
+    await this.octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/assignees",
+      {
+        owner: this.owner,
+        repo: this.repo,
+        issue_number: issueNumber,
+        assignees,
+      }
+    );
+  }
+
+  getIssuesFromCommits(commits: Commit[]): Set<string> {
+    const issues = new Set<string>();
+    for (const commit of commits) {
+      const issueMatch = commit.commit.message.match(/#(\d+)/g);
+      if (issueMatch) {
+        issueMatch.forEach((issue: string) => issues.add(issue));
+      }
+    }
+    core.debug(
+      `Issues found in commits: ${JSON.stringify(Array.from(issues))}`
+    );
+    return issues;
+  }
+
+  getCommittersFromCommits(commits: any[]): Set<string> {
+    const committers = new Set<string>();
+    for (const commit of commits) {
+      if (commit.author) {
+        committers.add(commit.author.login);
+      }
+    }
+    return committers;
+  }
+}
--- libs/recnet-release-action/src/lib/main.ts
+++ libs/recnet-release-action/src/lib/main.ts
@@ -1,80 +1,69 @@
 import * as core from "@actions/core";
 
 import { inputs } from "./env";
-// import { GitHubAPI } from "./github";
+import { GitHubAPI, PR } from "./github";
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  core.info("Hello, world!");
-
-  console.log(`Inputs: ${JSON.stringify(inputs)}`);
-  core.debug(`Inputs: ${JSON.stringify(inputs)}`);
-
   // TODO
   // 1. initalize a GitHub API client
-  // 2. get the diff between the base and target branches
-  // 3. if not diff, skip
-  // 4. find if there's already an opened PR from the base to target branch
-  // 5. if not, create a new PR
-  // 6. get the list of issues linked to the commits
-  // 7. update the PR content (put the list of issues to PR description)
-  // 8. find the committers of the commits
-  // 9. assign the PR to the committers and tag them as reviewers
-  // try {
-  //   core.info("Starting the RecNet release action");
+  // 2. find if there's already an opened PR from the base to target branch
+  // 3. if not, create a new PR
+  // 4. get the list of issues linked to the commits
+  // 5. update the PR content (put the list of issues to PR description)
+  // 6. find the committers of the commits
+  // 7. assign the PR to the committers and tag them as reviewers
+  try {
+    core.info("Starting the RecNet release action");
+    core.debug(`Load required inputs...`);
+    core.debug(`Inputs: ${JSON.stringify(inputs)}`);
 
-  //   const github = new GitHubAPI(inputs.githubToken, "", "inputs.repo");
+    const github = new GitHubAPI(inputs.githubToken, inputs.owner, inputs.repo);
 
-  //   // Get the diff between the base and target branches
-  //   const comparison = await github.getDiff(
-  //     inputs.baseBranch,
-  //     inputs.targetBranch
-  //   );
+    // Find if there's already an opened PR from the head to base branch created by this action
+    let pr: PR | null = null;
+    pr = await github.findPRCreatedByBot(inputs.baseBranch, inputs.headBranch);
 
-  //   // If no diff, skip
-  //   if ((comparison.files ?? []).length === 0) {
-  //     core.info("No differences found between branches. Skipping PR creation.");
-  //     return;
-  //   }
+    if (!pr) {
+      // If not, create a new PR
+      pr = await github.createPR(
+        `Release ${inputs.headBranch} to ${inputs.baseBranch}`, // PR title
+        inputs.baseBranch,
+        inputs.headBranch,
+        "Auto-generated release PR" // Initial PR body
+      );
+      core.info(`New PR created: #${pr.number}`);
+    } else {
+      core.info(`Existing PR found: #${pr.number}`);
+    }
 
-  //   // Find if there's already an opened PR from the base to target branch
-  //   let pr = await github.findPR(inputs.baseBranch, inputs.targetBranch);
+    // Get the latest commits from the head branch
+    const commits = await github.getLatestCommits(inputs.headBranch);
+    core.info(`Found ${commits.length} new commits`);
+    core.debug(`Commits: ${JSON.stringify(commits)}`);
 
-  //   if (!pr) {
-  //     // If not, create a new PR
-  //     pr = await github.createPR(
-  //       `Release ${inputs.targetBranch} to ${inputs.baseBranch}`,
-  //       inputs.baseBranch,
-  //       inputs.targetBranch,
-  //       "Auto-generated release PR"
-  //     );
-  //     core.info(`New PR created: #${pr.number}`);
-  //   } else {
-  //     core.info(`Existing PR found: #${pr.number}`);
-  //   }
+    // Get the list of issues linked to the commits
+    const issues = github.getIssuesFromCommits(commits);
+    core.info(`Found ${issues.size} linked issues`);
+    core.debug(`Issues: ${JSON.stringify(Array.from(issues))}`);
 
-  //   // Get the list of issues linked to the commits
-  //   const issues = github.getIssuesFromCommits(comparison.commits);
+    // // Update the PR content
+    // await github.appendIssuesToPR(pr, issues);
 
-  //   // Update the PR content (put the list of issues to PR description)
-  //   const issuesList = Array.from(issues).join(", ");
-  //   const updatedBody = `Auto-generated release PR\n\nRelated issues: ${issuesList}`;
-  //   await github.updatePR(pr.number, updatedBody);
+    // // Find the committers of the commits
+    // const committers = github.getCommittersFromCommits(commits);
 
-  //   // Find the committers of the commits
-  //   const committers = github.getCommittersFromCommits(comparison.commits);
+    // // Assign the PR to the committers and tag them as reviewers
+    // await github.requestReviewers(pr.number, Array.from(committers));
+    // await github.addAssignees(pr.number, Array.from(committers));
 
-  //   // Assign the PR to the committers and tag them as reviewers
-  //   await github.requestReviewers(pr.number, Array.from(committers));
-  //   await github.addAssignees(pr.number, Array.from(committers));
-
-  //   core.info("RecNet release action completed successfully");
-  // } catch (error) {
-  //   core.setFailed(
-  //     `Action failed with error: ${error instanceof Error ? error.message : String(error)}`
-  //   );
-  // }
+    core.info("RecNet release action completed successfully");
+  } catch (error) {
+    core.setFailed(
+      `Action failed with error: ${error instanceof Error ? error.message : String(error)}`
+    );
+  }
 }
