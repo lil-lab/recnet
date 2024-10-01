12699a97afda7e9134041090915925094635fbbc
swh00tw <a6140000@gmail.com>
2024-09-30 23:02:08 -0400

    chore: add comment

--- apps/recnet/src/clientEnv.ts
+++ apps/recnet/src/clientEnv.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
 function resolveBaseUrl(env: string | undefined) {
+  /**
+   * If the environment is preview, we need to use the Vercel branch URL.
+   * Otherwise, we use the base URL.
+   * Ref: https://vercel.com/docs/projects/environment-variables/framework-environment-variables#NEXT_PUBLIC_VERCEL_BRANCH_URL
+   */
   if (env === "preview") {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
   }
