bb40960e1bbab045d47d7a9bf84a8aa508c6c3d1
swh00tw <a6140000@gmail.com>
2024-11-07 13:57:12 -0500

    chore: export api types

--- libs/recnet-api-model/src/index.ts
+++ libs/recnet-api-model/src/index.ts
@@ -5,5 +5,6 @@ export * from "./lib/api/invite-code";
 export * from "./lib/api/rec";
 export * from "./lib/api/stat";
 export * from "./lib/api/user";
+export * from "./lib/api/subscription";
 export * from "./lib/model";
 export { generateMock } from "@anatine/zod-mock";
