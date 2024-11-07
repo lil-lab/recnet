d2e44adf0f15e7db584b191fe20eae2318284548
swh00tw <a6140000@gmail.com>
2024-11-07 14:09:47 -0500

    feat: finish subscription trpc procedures

---
+++ apps/recnet/src/server/routers/subscription.ts
@@ -0,0 +1,44 @@
+import {
+  getUsersSubscriptionsResponseSchema,
+  postUsersSubscriptionsRequestSchema,
+  postUsersSubscriptionsResponseSchema,
+} from "@recnet/recnet-api-model";
+
+import { checkRecnetJWTProcedure } from "./middleware";
+
+import { router } from "../trpc";
+
+export const subscriptionRouter = router({
+  getSubscriptions: checkRecnetJWTProcedure
+    .output(getUsersSubscriptionsResponseSchema)
+    .query(async (opts) => {
+      // TODO: after finish the API, use API call below
+      // const { recnetApi } = opts.ctx
+      // const { data } = await recnetApi.get("/users/subscriptions")
+      // return getUsersSubscriptionsResponseSchema.parse(data)
+
+      // Use mock data since the API is not ready
+      const mockData = getUsersSubscriptionsResponseSchema.parse({
+        subscriptions: ["WEEKLY_DIGEST"],
+        channels: ["EMAIL"],
+      });
+      return mockData;
+    }),
+  updateSubscription: checkRecnetJWTProcedure
+    .input(postUsersSubscriptionsRequestSchema)
+    .output(postUsersSubscriptionsResponseSchema)
+    .mutation(async (opts) => {
+      // TODO: after finish the API, use API call below
+      const { subscription } = opts.input;
+      // const { recnetApi } = opts.ctx;
+      // const { data } = await recnetApi.post("/users/subscriptions", {
+      //   subscription: subscription,
+      // });
+      // return postUsersSubscriptionsResponseSchema.parse(data);
+
+      // Use mock data(whatever user input) since the API is not ready
+      return postUsersSubscriptionsResponseSchema.parse({
+        subscription,
+      });
+    }),
+});
