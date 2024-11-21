894daa0e292e538d369dd425fd092f59fe6361d2
swh00tw <a6140000@gmail.com>
2024-11-21 18:07:20 -0500

    chore: remove comment and log

--- apps/recnet/src/app/api/slack/oauth/callback/route.ts
+++ apps/recnet/src/app/api/slack/oauth/callback/route.ts
@@ -7,14 +7,12 @@ export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const code = searchParams.get("code");
   const errorDesc = searchParams.get("error_description");
-  console.log("errorDesc: ", errorDesc);
 
   if (!code) {
     redirect(
       `/feeds?slackOAuthStatus=error${errorDesc ? `&error_description=${errorDesc}` : ""}`
     );
   }
-  // hit trpc api to forward the code to api server
   let isSuccess = true;
   try {
     await serverClient.slackOAuth2FA({ code });
