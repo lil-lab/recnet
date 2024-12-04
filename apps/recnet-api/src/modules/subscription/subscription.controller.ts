6074bf07af8372aed986ca34a7272840f5cc9d7a
swh00tw <a6140000@gmail.com>
2024-12-04 13:46:04 -0500

    fix: change from field of email

--- apps/recnet-api/src/modules/subscription/subscription.controller.ts
+++ apps/recnet-api/src/modules/subscription/subscription.controller.ts
@@ -19,7 +19,32 @@ import { announcementSchema, recSchema } from "@recnet/recnet-api-model";
 
 import { WeeklyDigestContent } from "./subscription.type";
 
+import { EmailService } from "../email/email.service";
 import { SlackService } from "../slack/slack.service";
+
+function getMockWeeklyDigestData(): WeeklyDigestContent {
+  const getMockRec = (title = 1) =>
+    generateMock(recSchema, {
+      stringMap: {
+        photoUrl: () => "https://avatar.iran.liara.run/public",
+        title: () => `Paper Title ${title}`,
+      },
+    });
+  const announcement = generateMock(announcementSchema, {
+    stringMap: {
+      content: () => "This is a test announcement!",
+    },
+  });
+  return {
+    recs: [getMockRec(), getMockRec(2), getMockRec(3), getMockRec()],
+    numUnusedInviteCodes: 3,
+    latestAnnouncement: {
+      ...announcement,
+      startAt: new Date(announcement.startAt),
+      endAt: new Date(announcement.endAt),
+    },
+  };
+}
 
 @ApiTags("subscriptions")
 @Controller("subscriptions")
@@ -28,6 +53,7 @@ export class SubscriptionController {
     @Inject(AppConfig.KEY)
     private readonly appConfig: ConfigType<typeof AppConfig>,
     private readonly slackService: SlackService,
+    private readonly emailService: EmailService,
     private readonly userRepository: UserRepository
   ) {}
 
@@ -46,7 +72,7 @@ export class SubscriptionController {
     },
   })
   @Post("slack/test")
-  public async testSendingWeeklyDigest(
+  public async testSendingSlackWeeklyDigest(
     @Body("userId") userId: string
   ): Promise<void> {
     if (this.appConfig.nodeEnv === "production") {
@@ -57,34 +83,42 @@ export class SubscriptionController {
       );
     }
 
-    function getMockWeeklyDigestData(): WeeklyDigestContent {
-      const getMockRec = (title = 1) =>
-        generateMock(recSchema, {
-          stringMap: {
-            photoUrl: () => "https://avatar.iran.liara.run/public",
-            title: () => `Paper Title ${title}`,
-          },
-        });
-      const announcement = generateMock(announcementSchema, {
-        stringMap: {
-          content: () => "This is a test announcement!",
-        },
-      });
-      return {
-        recs: [getMockRec(), getMockRec(2), getMockRec(3), getMockRec()],
-        numUnusedInviteCodes: 3,
-        latestAnnouncement: {
-          ...announcement,
-          startAt: new Date(announcement.startAt),
-          endAt: new Date(announcement.endAt),
-        },
-      };
+    const cutoff = getLatestCutOff();
+    const user = await this.userRepository.findUserById(userId);
+    const content = getMockWeeklyDigestData();
+
+    this.slackService.sendWeeklyDigest(user, content, cutoff);
+  }
+
+  @ApiOperation({
+    summary: "[Dev only] Send weekly digest slack to the designated user.",
+    description: "This endpoint is for development only.",
+  })
+  @ApiCreatedResponse()
+  @ApiBody({
+    schema: {
+      properties: {
+        userId: { type: "string" },
+      },
+      required: ["userId"],
+    },
+  })
+  @Post("email/test")
+  public async testSendingEmailWeeklyDigest(
+    @Body("userId") userId: string
+  ): Promise<void> {
+    if (this.appConfig.nodeEnv === "production") {
+      throw new RecnetError(
+        ErrorCode.INTERNAL_SERVER_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "This endpoint is only for development"
+      );
     }
 
     const cutoff = getLatestCutOff();
     const user = await this.userRepository.findUserById(userId);
     const content = getMockWeeklyDigestData();
 
-    this.slackService.sendWeeklyDigest(user, content, cutoff);
+    this.emailService.sendWeeklyDigest(user, content, cutoff);
   }
 }
